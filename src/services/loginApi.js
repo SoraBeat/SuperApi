890829d7568dbb72bacc84d04c7b32ad35d3e3e9
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2022-09-30 20:53:28 -0300

    Login fix

--- src/services/loginApi.js
+++ src/services/loginApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export  function loginApi(email, password) {
-const response =  axios.post("https://challenge-react.alkemy.org", {email,password})
+const response =  axios.post("http://challenge-react.alkemy.org", {email,password})
   .then(function (response) {
     return response
   })
